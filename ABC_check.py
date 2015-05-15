fa92b3292d695e41bab2574e923dbbfd9a8590bd
agvanwin <aaron.vanwinkle13@gmail.com>
2015-05-14 23:35:13 -0400

    Update ABC_check.py

--- ABC_check.py
+++ ABC_check.py
@@ -1,18 +1,20 @@
 def abc_store_check():
+    
     print "Welcome to the ABC age checker"
+    
     print "Please enter your age"
     answer = int(raw_input())
     
-    if answer >= 21:
-        print "Congratulations, you are old enough to shop at the ABC store."
-        return True
+    if answer < 21:
+        print "Sorry, you are not old enough to shop at the ABC store"
+        return False
         
-    elif answer < 21:
-        print "Sorry, you are not old enough to shop at the ABC store."
+    elif answer > 21:
+        print "Congratulations, you may shop at the ABC store."
         return True
     
-    else:
-        print "You must enter your age!"
+    elif answer == 21:
+        print "Good try McLovin...."
         return True
         
         abc_store_check()
